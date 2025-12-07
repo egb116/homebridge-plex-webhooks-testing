95f80b502e1a32c30e30edcedd7221008ee037dc
Eric Bowers <ebowers@gmail.com>
2025-12-07 10:43:15 -0500

    Fix import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { PlexWebhooksPlatform } = require('./src/platform');
+const PlexWebhooksPlatform = require('./src/platform');
 const { PLATFORM_NAME } = require('./src/settings');
 
 /**
